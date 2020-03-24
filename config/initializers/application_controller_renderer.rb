2c2fa5b7cd880408dbc259fe44e487d93ac43f12
古田光流 <m1212r@furutahikariryuunoMacBook-Air.local>
2020-03-24 16:00:49 +0900

    Revert "Revert "Revert "first commit"""
    This reverts commit 79ed51af1e847dd42fea3e4c303b64c65058383b.

--- config/initializers/application_controller_renderer.rb
+++
@@ -1,9 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-# end
-Refile.secret_key = '01889c8ba3f2c6763de5704b6172f0a3615d5c9bf6abffb13281e6e2a0c605e64293dfe21c40cb6fd8ce7de5ea0d05d8b544d84dc6120f50fb45510bed3231cd'
